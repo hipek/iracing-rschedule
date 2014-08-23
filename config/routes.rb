71ec8aa79b9250bb939b1fe72e55bfe5387d8df8
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2014-08-23 17:40:41 +0200

    Add production scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,25 @@
 Rails.application.routes.draw do
-  resources :teams
-  resources :seasons
-  resources :tracks
-  resources :series, only: [:edit, :update, :index, :show]
+  scope Rails.application.config.relative_url_root do
+    resources :teams
+    resources :seasons
+    resources :tracks
+    resources :series, only: [:edit, :update, :index, :show]
 
-  get 'welcome/index'
+    get 'welcome/index'
 
-  root 'welcome#index'
+    root 'welcome#index'
 
-  post 'series_track_values/:id', to: 'srs_track_values#update'
+    post 'series_track_values/:id', to: 'srs_track_values#update'
 
-  scope path: ':team_id' do
-    resources :user_seasons
+    scope path: ':team_id' do
+      resources :user_seasons
 
-    resources :users
+      resources :users
 
-    get 'home/index'
-    get 'my_series/:id', to: 'home#show', as: :my_series
+      get 'home/index'
+      get 'my_series/:id', to: 'home#show', as: :my_series
 
-    get '/', to: 'home#index', as: :team_root
+      get '/', to: 'home#index', as: :team_root
+    end
   end
 end
