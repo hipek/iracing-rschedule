e72ae87a8285c2a8f5f267872bb5d97453948234
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2014-10-19 12:33:54 +0200

    Update dest server info

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'ischedule'
-set :repo_url, 'git@jac.zapto.org:ruby/ischedule.git'
+set :repo_url, 'git@git.jacekhiszpanski.tk:ruby/ischedule.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
