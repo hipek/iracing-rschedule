dc94dcf63152aa61527af52c7b38fb187b6868f2
Jacek Hiszpanski <j.hiszpanski@gmail.com>
2014-12-24 14:20:29 +0100

    Upgrade rails and turbolinks gems

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 # ruby '2.1.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.6'
+gem 'rails', '4.2.0'
 
 gem 'pg'
 
@@ -21,7 +21,7 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+gem 'turbolinks', '~> 2.3.0'
 gem 'jquery-turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -16,66 +16,66 @@ ActiveRecord::Schema.define(version: 20140727175330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "seasons", force: true do |t|
-    t.string   "name"
+  create_table "seasons", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "active",     default: false
+    t.boolean  "active",                 default: false
   end
 
-  create_table "series", force: true do |t|
-    t.string   "name"
+  create_table "series", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.integer  "season_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "license"
-    t.string   "race_type"
-    t.string   "cars",       default: [], array: true
+    t.string   "license",    limit: 255
+    t.string   "race_type",  limit: 255
+    t.string   "cars",                   default: [], array: true
   end
 
-  create_table "series_tracks", force: true do |t|
+  create_table "series_tracks", force: :cascade do |t|
     t.integer  "series_id"
     t.integer  "track_id"
-    t.string   "name"
+    t.string   "name",       limit: 255
     t.date     "date"
     t.integer  "week"
-    t.string   "duration"
+    t.string   "duration",   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "night",      default: false
+    t.boolean  "night",                  default: false
   end
 
-  create_table "srs_track_values", force: true do |t|
+  create_table "srs_track_values", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "series_track_id"
-    t.string   "value"
+    t.string   "value",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "teams", force: true do |t|
-    t.string   "name"
-    t.string   "slug"
+  create_table "teams", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "slug",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "tracks", force: true do |t|
-    t.string   "name"
+  create_table "tracks", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "user_seasons", force: true do |t|
+  create_table "user_seasons", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "season_id"
-    t.string   "series_names"
+    t.string   "series_names", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "name"
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "track_ids"
