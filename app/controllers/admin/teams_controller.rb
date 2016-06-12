7a77d5638cbd2db4b1b93fc0c3f2c462f460b2c0
Jacek Hiszpanski <jacek.hiszpanski@tooploox.com>
2016-06-12 22:48:25 +0200

    Update admin teams sesion

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -28,7 +28,7 @@ class Admin::TeamsController < ApplicationController
 
     respond_to do |format|
       if @team.save
-        format.html { redirect_to @team, notice: 'Team was successfully created.' }
+        format.html { redirect_to [:admin, @team], notice: 'Team was successfully created.' }
         format.json { render :show, status: :created, location: @team }
       else
         format.html { render :new }
