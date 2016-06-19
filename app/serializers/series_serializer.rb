136fd5e16fa47361356a02294b425caa6dae73d4
Jacek Hiszpanski <jacek.hiszpanski@tooploox.com>
2016-06-19 10:30:51 +0200

    Update json serializers

--- app/serializers/series_serializer.rb
+++ app/serializers/series_serializer.rb
@@ -1,4 +1,4 @@
 class SeriesSerializer < ActiveModel::Serializer
-  attributes :id, :name, :license, :race_type, :cars, :created_at
+  attributes :id, :name, :license, :race_type, :cars
   has_many :series_tracks
 end
--- app/serializers/series_track_serializer.rb
+++ app/serializers/series_track_serializer.rb
@@ -1,3 +1,3 @@
 class SeriesTrackSerializer < ActiveModel::Serializer
-  attributes :id, :week, :date, :duration, :name, :time_of_day, :created_at
+  attributes :id, :week, :date, :duration, :name, :time_of_day
 end
